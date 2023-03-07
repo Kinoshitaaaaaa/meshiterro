cd79c4b7a211f7f418600c2d8c787e10f108d0ef
EC2 Default User <ec2-user@ip-172-31-33-205.ap-northeast-1.compute.internal>
2023-03-07 16:55:37 +0000

    [update2]4-1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def about
+
+  end
+
+  def create
+
+  end
+
+end
